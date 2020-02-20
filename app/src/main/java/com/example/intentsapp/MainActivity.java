44cf63ac6ff4d8fb01da69b5a69bcd8d98b5d57b
mbadaz <tapsmuzira@gmail.com>
2020-02-21 01:12:57 +0200

    Launch explicit and implicit intent

--- app/src/main/java/com/example/intentsapp/MainActivity.java
+++ app/src/main/java/com/example/intentsapp/MainActivity.java
@@ -2,28 +2,53 @@ package com.example.intentsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String ARG = "text";
+    private EditText inputEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        EditText inputEditText = findViewById(R.id.edtInput);
+        inputEditText = findViewById(R.id.edtInput);
         Button launchButton = findViewById(R.id.btnLaunch);
+        launchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             //   launchInternalActivity();
+                launchExternalActivity();
+            }
+        });
 
     }
 
     private void launchInternalActivity() {
         // TODO - Create explicit intent and start second activity
+        String text = inputEditText.getText().toString();
+        Intent intent = new Intent(this, SecondActivity.class);
+        intent.putExtra(ARG, text);
 
+        startActivity(intent);
     }
 
     private void launchExternalActivity() {
-        // TODO - Create explicit intent and launch external activity
+        // TODO - Create implicit intent and launch external email app
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        String[] emails = {"JohnDoe@example.com", "foo@bar.com"};
+        intent.putExtra(Intent.EXTRA_EMAIL, emails);
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Hello World!");
 
+        // Verify if there is an app that can fulfill the intent before launching
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/intentsapp/SecondActivity.java
+++ app/src/main/java/com/example/intentsapp/SecondActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -12,7 +13,9 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         TextView textView = findViewById(R.id.txtShowText);
-
         // TODO - Get passed information from intent bundle.
+        String text = getIntent().getStringExtra(MainActivity.ARG);
+        textView.setText(text);
+        Toast.makeText(this, "First Activity says:" + text, Toast.LENGTH_SHORT).show();
     }
 }
