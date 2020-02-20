b97dc748a2862305b28130317badc58509f1ba04
mbadaz <tapsmuzira@gmail.com>
2020-02-21 00:37:09 +0200

    Start

--- app/src/main/java/com/example/intentsapp/MainActivity.java
+++ app/src/main/java/com/example/intentsapp/MainActivity.java
@@ -15,12 +15,15 @@ public class MainActivity extends AppCompatActivity {
         EditText inputEditText = findViewById(R.id.edtInput);
         Button launchButton = findViewById(R.id.btnLaunch);
 
+    }
 
+    private void launchInternalActivity() {
+        // TODO - Create explicit intent and start second activity
 
     }
 
-    private void launchIntent() {
-        // TODO - Create intent and start second activity
+    private void launchExternalActivity() {
+        // TODO - Create explicit intent and launch external activity
 
     }
 }
