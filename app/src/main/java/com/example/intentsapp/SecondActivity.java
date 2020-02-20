2dccf9a142f210d73c922e43c5a56df836b74846
mbadaz <tapsmuzira@gmail.com>
2020-02-21 00:31:11 +0200

    Start

---
+++ app/src/main/java/com/example/intentsapp/SecondActivity.java
@@ -0,0 +1,18 @@
+package com.example.intentsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        TextView textView = findViewById(R.id.txtShowText);
+
+        // TODO - Get passed information from intent bundle.
+    }
+}
